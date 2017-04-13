b9738eb834f53b7f8d33ade4cc7f5af611d090a0
bxh9261 <bxh9261@g.rit.edu>
2017-04-13 17:23:44 -0400

    Update AnnabelleAndAidan.java

--- AnnabelleAndAidan.java
+++ AnnabelleAndAidan.java
@@ -3,12 +3,10 @@ package apples2017;
 public class AnnabelleAndAidan extends CardPack{
 	
 	/**
-	 * Creates a new MrsMillersClass card pack, with an array of Strings for red cards and 
-	 * another for green cards.
+	 * Creates a new AnnabelleAndAidan card pack, with an array of Strings for red cards
 	 */
 	public AnnabelleAndAidan(){
-		redCards = new String[]{"Annabelle", "Aidan", "Brad", "Eva", "Ashton", " Brad ", " Annabelle "};
-		greenCards = new String[]{"_____ is awesome", "______ is the coolest of these people", "_______ smells", "______ has swag.", "_______ might be the coolest but idk lol"
+		redCards = new String[]{"Annabelle", "Aidan", "Brad", "Eva", "Ashton"};
 		};
 	}
 	
